3e63edd827291fcdbc4d4a8cbaa25642727ac702
abensouira <abensouira@takima.fr>
2025-03-28 14:21:27 +0100

    first commit

---
+++ sample1/Personne.cs
@@ -0,0 +1,22 @@
+public class Personne
+{
+    public string Nom { get; set; }
+    public int Age { get; set; }
+
+    public string Hello(bool isLowerCase = true) {
+        if (!isLowerCase)
+        {
+            return $"Hello {Nom}, you are {Age} years old".ToUpper();
+        }
+        else
+        {
+            return $"Hello {Nom}, you are {Age} years old".ToLower();
+        }
+    }
+
+    public Personne(string nom, int age)
+    {
+        Nom = nom;
+        Age = age;
+    }
+}
---
+++ sample1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace sample1;
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        Personne personne = new Personne("Jean", 42);
+        Console.WriteLine(personne.Hello());
+        Console.WriteLine(personne.Hello(false));
+    }
+}
